70d9d44a43eb681544b3c6bb86d60c7c2637388b
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2023-01-22 22:56:50 +0100

    Added package-level nullability annotations to bean packages

---
+++ src/main/java/io/jacopocav/customercare/repository/package-info.java
@@ -0,0 +1,6 @@
+@NonNullApi
+@NonNullFields
+package io.jacopocav.customercare.repository;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
---
+++ src/main/java/io/jacopocav/customercare/service/package-info.java
@@ -0,0 +1,6 @@
+@NonNullApi
+@NonNullFields
+package io.jacopocav.customercare.service;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
