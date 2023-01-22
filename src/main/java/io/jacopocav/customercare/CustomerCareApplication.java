6a9546f47cadad972f370aeab829208c67890f31
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2023-01-22 12:07:12 +0100

    Created CustomerCareApplication entrypoint for Spring Boot

---
+++ src/main/java/io/jacopocav/customercare/CustomerCareApplication.java
@@ -0,0 +1,11 @@
+package io.jacopocav.customercare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerCareApplication.class, args);
+    }
+}
