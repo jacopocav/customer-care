52894ed23b56cb7c90a6c5421cd301aa84468308
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2023-01-24 21:39:18 +0100

    Added Device entity and Customer - Device relationship

--- src/main/java/io/jacopocav/customercare/model/Customer.java
+++ src/main/java/io/jacopocav/customercare/model/Customer.java
@@ -1,11 +1,15 @@
 package io.jacopocav.customercare.model;
 
+import static jakarta.persistence.FetchType.LAZY;
+
+import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +28,6 @@ public class Customer extends CommonModel {
     private String fiscalCode;
     @Column(nullable = false)
     private String address;
+    @OneToMany(fetch = LAZY)
+    private List<Device> devices;
 }
