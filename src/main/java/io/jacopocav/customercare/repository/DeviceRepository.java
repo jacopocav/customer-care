594dedf79378822725129f1404428a95b8528f6d
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2023-01-24 21:55:51 +0100

    Added interfaces for Device-related components

---
+++ src/main/java/io/jacopocav/customercare/repository/DeviceRepository.java
@@ -0,0 +1,10 @@
+package io.jacopocav.customercare.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.jacopocav.customercare.model.Device;
+
+public interface DeviceRepository extends JpaRepository<Device, UUID> {
+}
---
+++ src/main/java/io/jacopocav/customercare/service/DeviceCrudService.java
@@ -0,0 +1,18 @@
+package io.jacopocav.customercare.service;
+
+import java.util.UUID;
+
+import io.jacopocav.customercare.dto.CreateDeviceRequest;
+import io.jacopocav.customercare.dto.ReadDeviceResponse;
+import io.jacopocav.customercare.dto.UpdateDeviceRequest;
+import jakarta.validation.constraints.NotBlank;
+
+public interface DeviceCrudService {
+    UUID create(CreateDeviceRequest request);
+
+    ReadDeviceResponse read(@NotBlank String id);
+
+    void update(@NotBlank String id, UpdateDeviceRequest request);
+
+    void delete(@NotBlank String id);
+}
