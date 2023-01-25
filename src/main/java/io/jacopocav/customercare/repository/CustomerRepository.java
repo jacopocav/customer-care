3d84f5984a919d8e3a2119d871a6d69d035d5740
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2023-01-25 18:33:33 +0100

    Added findByIdFetchingDevices to CustomerRepository

--- src/main/java/io/jacopocav/customercare/repository/CustomerRepository.java
+++ src/main/java/io/jacopocav/customercare/repository/CustomerRepository.java
@@ -1,10 +1,17 @@
 package io.jacopocav.customercare.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import io.jacopocav.customercare.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    @Query("select c "
+        + "from Customer c "
+        + "join fetch c.devices "
+        + "where c.id = :id")
+    Optional<Customer> findByIdFetchingDevices(UUID id);
 }
